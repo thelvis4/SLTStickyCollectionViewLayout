5f24de0dd20c87eebe26fe1e5903f55741e5feb8
Andrei Raifura <andrei.raifura@yopeso.com>
2015-04-01 11:35:09 +0300

    Initial Commit

---
+++ SLTStickyCollectionViewLayout/SLTStickyCollectionViewLayout.h
@@ -0,0 +1,13 @@
+//
+//  SLTStickyCollectionViewLayout.h
+//  SLTStickyCollectionViewLayout
+//
+//  Created by Andrei Raifura on 4/1/15.
+//  Copyright (c) 2015 YOPESO. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLTStickyCollectionViewLayout : NSObject
+
+@end
---
+++ SLTStickyCollectionViewLayout/SLTStickyCollectionViewLayout.m
@@ -0,0 +1,13 @@
+//
+//  SLTStickyCollectionViewLayout.m
+//  SLTStickyCollectionViewLayout
+//
+//  Created by Andrei Raifura on 4/1/15.
+//  Copyright (c) 2015 YOPESO. All rights reserved.
+//
+
+#import "SLTStickyCollectionViewLayout.h"
+
+@implementation SLTStickyCollectionViewLayout
+
+@end
